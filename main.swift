fed589d59cc6995f2c54834de4635929a241fcde
Muhammad Luthfi Farizqi Luthfi <luthfifarizqi23@gmail.com>
2020-10-21 04:10:16 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,39 @@
+//Switch statements
+/*
+let weather = "sunny"
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+default:
+    print("Enjoy your day!")
+}
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+    fallthrough
+default:
+    print("Enjoy your day!")
+}
+*/
+//switch cuaca
+let cuaca = "musim hujan"
+
+switch cuaca{
+  case "musim kemarau":
+  print("saya mau liburan")
+  case "musim gugur":
+  print("saya pergi  ke jepang")
+  case "musim hujan":
+  print("saya mau di rumah saja")
+  fallthrough
+  default :
+  print(" Semua musim sama aja")
+}
